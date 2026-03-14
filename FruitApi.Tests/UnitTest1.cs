48d22fc71423d3bb4ffab7d30e3a47e7fb35decc
AntoR25 <antoine.remacle2407@gmail.com>
2026-03-14 10:07:40 +0100

    ajout de test

---
+++ FruitApi.Tests/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using Xunit;
+using FruitWebApp.Components.Pages;
+using Moq;
+using System.Net.Http;
+
+public class HomeTests
+{
+    [Fact]
+    public void Home_Component_Creation_Test()
+    {
+        // Création d'un HttpClientFactory factice
+        var mockFactory = new Mock<IHttpClientFactory>();
+        mockFactory.Setup(f => f.CreateClient(It.IsAny<string>())).Returns(new HttpClient());
+
+        // Création du composant en injectant la factory
+        var home = new Home
+        {
+            HttpClientFactory = mockFactory.Object
+        };
+
+        // Vérifie que l'objet existe
+        Assert.NotNull(home);
+    }
+}
